e992b69516c9759c99c17972d08bdff402018bec
Apple Coding <academy@applecoding.com>
2019-06-03 12:48:31 +0200

    activiy mostra coches

---
+++ app/src/main/java/edu/cftic/sql_app/MuestraCochesActivity.java
@@ -0,0 +1,13 @@
+package edu.cftic.sql_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MuestraCochesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_muestra_coches);
+    }
+}
